e357bedc3494c6efd4db3ad1fdabd09aa1abdaeb
i0 Dev <AndrewOMagnuson@gmail.com>
2021-08-03 00:59:36 -0500

    Rearanged things & changed the message that sends emphereially to not be embeded.

--- src/main/java/com/i0dev/modules/misc/VerifyHandler.java
+++ src/main/java/com/i0dev/modules/misc/VerifyHandler.java
@@ -1,5 +1,6 @@
 package com.i0dev.modules.misc;
 
+import com.i0dev.Bot;
 import com.i0dev.config.MiscConfig;
 import com.i0dev.modules.moderation.CmdVerifyPanel;
 import com.i0dev.object.EmbedColor;
@@ -8,12 +9,14 @@ import com.i0dev.object.Type;
 import com.i0dev.object.discordLinking.DPlayer;
 import com.i0dev.utility.EmbedMaker;
 import com.i0dev.utility.MessageUtil;
+import com.i0dev.utility.TimeUtil;
 import com.i0dev.utility.Utility;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class VerifyHandler extends ListenerAdapter {
+import java.util.concurrent.TimeUnit;
 
+public class VerifyHandler extends ListenerAdapter {
 
     @Override
     public void onButtonClick(ButtonClickEvent e) {
@@ -23,11 +26,9 @@ public class VerifyHandler extends ListenerAdapter {
         if (!Utility.isValidGuild(e.getGuild())) return;
         DPlayer dPlayer = DPlayer.getDPlayer(e.getUser());
         if (dPlayer.isBlacklisted()) return;
-
-        MiscConfig.get().verify_giveRoles.forEach(roleID -> new RoleQueueObject(e.getUser().getIdLong(), roleID, Type.ADD_ROLE));
-        MiscConfig.get().verify_removeRoles.forEach(roleID -> new RoleQueueObject(e.getUser().getIdLong(), roleID, Type.REMOVE_ROLE));
-
         MessageUtil.sendPrivateMessage(e.getMessage(), e.getUser(), EmbedMaker.builder().authorName(CmdVerifyPanel.getOption("title", CmdVerifyPanel.class).getAsString()).authorImg(e.getUser().getEffectiveAvatarUrl()).embedColor(EmbedColor.SUCCESS).content("You have successfully verified yourself in the **{guildName}** discord!".replace("{guildName}", e.getGuild().getName())).build());
-        e.getInteraction().deferReply(true).addEmbeds(EmbedMaker.create(EmbedMaker.builder().embedColor(EmbedColor.SUCCESS).content("You have successfully verified yourself.").build())).queue();
+        e.getInteraction().deferReply(true).setContent("You have successfully verified yourself.").queue();
+        MiscConfig.get().verify_removeRoles.forEach(roleID -> new RoleQueueObject(e.getUser().getIdLong(), roleID, Type.REMOVE_ROLE).add());
+        MiscConfig.get().verify_giveRoles.forEach(roleID -> new RoleQueueObject(e.getUser().getIdLong(), roleID, Type.ADD_ROLE).add());
     }
 }
