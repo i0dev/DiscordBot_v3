1640b34a96417dbeeb91f351ae2d7159b73b4202
i0 Dev <AndrewOMagnuson@gmail.com>
2021-08-12 17:42:58 -0500

    Added a resync commnad to fully refresh a usrs profile link info, & ign

---
+++ src/main/java/com/i0dev/modules/linking/Resync.java
@@ -0,0 +1,36 @@
+package com.i0dev.modules.linking;
+
+import com.i0dev.object.CommandData;
+import com.i0dev.object.CommandEvent;
+import com.i0dev.object.EmbedColor;
+import com.i0dev.object.SuperDiscordCommand;
+import com.i0dev.object.discordLinking.DPlayer;
+import com.i0dev.utility.*;
+import net.dv8tion.jda.api.entities.User;
+
+public class Resync extends SuperDiscordCommand {
+
+    @CommandData(commandID = "resync", identifier = "Link Re-Sync", usage = "<user>", messageLength = 2, parentClass = LinkManager.class)
+    public static void run(CommandEvent e) {
+        User user;
+        if ((user = FindUtil.getUser(e.getOffsetSplit().get(1), e.getMessage())) == null) return;
+        DPlayer dPlayer = DPlayer.getDPlayer(user);
+        if (!dPlayer.isLinked()) {
+            e.reply(EmbedMaker.builder().embedColor(EmbedColor.FAILURE).user(user).content("{tag} is not linked to any account.").build());
+            return;
+        }
+
+        RoleRefreshHandler.RefreshUserRank(dPlayer);
+
+        e.reply(EmbedMaker.builder().embedColor(EmbedColor.SUCCESS).user(user).content("You have successfully refreshed {tag}'s link info").build());
+
+        String ign = APIUtil.getIGNFromUUID(dPlayer.getMinecraftUUID());
+        if (ign == null) return;
+        LinkData linkData = LinkData.getLinkData(dPlayer.getDiscordID());
+        linkData.setMinecraftIGN(ign);
+        linkData.save();
+        dPlayer.setLastUpdatedMillis(System.currentTimeMillis());
+        dPlayer.save();
+
+    }
+}
