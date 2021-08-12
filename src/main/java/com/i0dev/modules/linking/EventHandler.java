2812cf858b22b754bf2c9afe64c6e2e55bb64d87
i0 Dev <AndrewOMagnuson@gmail.com>
2021-08-12 17:40:27 -0500

    ADded an event handler for linking, to refresh the users rank when they join the server

---
+++ src/main/java/com/i0dev/modules/linking/EventHandler.java
@@ -0,0 +1,15 @@
+package com.i0dev.modules.linking;
+
+import com.i0dev.object.discordLinking.DPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+
+public class EventHandler implements Listener {
+    @net.md_5.bungee.event.EventHandler
+    public void onJoin(PostLoginEvent e) {
+        DPlayer dPlayer = DPlayer.getDPlayerFromUUID(e.getPlayer().getUniqueId().toString());
+        if (dPlayer == null) return;
+        if (!LinkData.getLinkData(dPlayer.getDiscordID()).isLinked()) return;
+        RoleRefreshHandler.RefreshUserRank(dPlayer);
+    }
+}
