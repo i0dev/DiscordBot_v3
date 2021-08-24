ab3d294ab8427790c3a6cba247ae58288eff38e2
i0 Dev <AndrewOMagnuson@gmail.com>
2021-08-24 15:27:30 -0500

    Added checks in help to filter out commands or sub-commands that are not enabled.

--- src/main/java/com/i0dev/modules/basic/CmdHelp.java
+++ src/main/java/com/i0dev/modules/basic/CmdHelp.java
@@ -5,27 +5,19 @@ import com.i0dev.config.CustomCommandsConfig;
 import com.i0dev.config.GeneralConfig;
 import com.i0dev.object.*;
 import com.i0dev.utility.EmbedMaker;
-import com.i0dev.utility.FindUtil;
-import com.i0dev.utility.Utility;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class CmdHelp extends DiscordCommand {
 
-    @CommandData(commandID = "cmd_help", identifier = "Help", maxMessageLength = 2, canBePrivateMessage = true)
+    @CommandData(commandID = "cmd_help", identifier = "Help", messageLength = 1, canBePrivateMessage = true)
     public static void run(CommandEvent e) {
-        Integer page = 1;
-        if (e.getSplit().length > 1) {
-            if ((page = FindUtil.getInteger(e.split[1], e.getMessage())) == null) return;
-        }
-
         String prefix = GeneralConfig.get().getPrefixes().get(0);
-
-
         StringBuilder basicCommands = new StringBuilder();
         basicCommands.append("__**Commands**__").append("\n");
         for (BasicCommand registeredCommand : Bot.getRegisteredCommands().stream().filter(command -> !(command instanceof AdvancedCommand)).collect(Collectors.toList())) {
+            if (!registeredCommand.isEnabled()) continue;
             basicCommands.append("`").append(prefix).append(registeredCommand.getAliases().get(0)).append("`").append(", ");
         }
         basicCommands.delete(basicCommands.length() - 2, basicCommands.length() - 1);
@@ -33,17 +25,24 @@ public class CmdHelp extends DiscordCommand {
         basicCommands.append("\n\n__**Advanced Commands**__").append("\n");
         for (BasicCommand registeredCommand : Bot.getRegisteredCommands().stream().filter(command -> (command instanceof AdvancedCommand)).collect(Collectors.toList())) {
             StringBuilder internal = new StringBuilder();
-            AdvancedDiscordCommand.getAdvancedCommand(registeredCommand.getClazz()).getSuperCommands().forEach(superCommand -> internal.append(superCommand.getAliases().get(0)).append(" | "));
+            if (!registeredCommand.isEnabled()) continue;
+            AdvancedDiscordCommand.getAdvancedCommand(registeredCommand.getClazz()).getSuperCommands().forEach(superCommand -> {
+                if (!superCommand.isEnabled()) return;
+                internal.append(superCommand.getAliases().get(0)).append(" | ");
+            });
             internal.delete(internal.length() - 3, internal.length());
             basicCommands.append("`").append(prefix).append(registeredCommand.getAliases().get(0)).append(" (").append(internal).append(")`").append("\n");
         }
 
-        basicCommands.append("\n__**Custom Commands**__").append("\n");
-        for (CustomCommandsConfig.MessageSetting cmd : CustomCommandsConfig.get().getCustomCommands()) {
-            basicCommands.append("`" + prefix + cmd.getCallers().get(0) + "`, ");
+        List<CustomCommandsConfig.MessageSetting> list = CustomCommandsConfig.get().getCustomCommands().stream().filter(messageSetting -> !messageSetting.getCallers().get(0).equals("Example Custom Message")).collect(Collectors.toList());
+        if (!list.isEmpty()) {
+            basicCommands.append("\n__**Custom Commands**__").append("\n");
+            for (CustomCommandsConfig.MessageSetting cmd : list) {
+                basicCommands.append("`").append(prefix).append(cmd.getCallers().get(0)).append("`, ");
+            }
+            basicCommands.delete(basicCommands.length() - 2, basicCommands.length() - 1);
         }
-        basicCommands.delete(basicCommands.length() - 2, basicCommands.length() - 1);
-
+        
 
         e.reply(EmbedMaker.builder().author(e.getAuthor()).authorName("DiscordBot Help Page").authorImg(Bot.getJda().getSelfUser().getEffectiveAvatarUrl()).content(basicCommands.toString()).build());
 
