35cd9f56d3390317892131b33350acb0029fc0d3
i0 Dev <AndrewOMagnuson@gmail.com>
2021-08-24 12:44:05 -0500

    Added CustomCommands to the help page!

--- src/main/java/com/i0dev/modules/basic/CmdHelp.java
+++ src/main/java/com/i0dev/modules/basic/CmdHelp.java
@@ -1,6 +1,7 @@
 package com.i0dev.modules.basic;
 
 import com.i0dev.Bot;
+import com.i0dev.config.CustomCommandsConfig;
 import com.i0dev.config.GeneralConfig;
 import com.i0dev.object.*;
 import com.i0dev.utility.EmbedMaker;
@@ -19,11 +20,13 @@ public class CmdHelp extends DiscordCommand {
             if ((page = FindUtil.getInteger(e.split[1], e.getMessage())) == null) return;
         }
 
+        String prefix = GeneralConfig.get().getPrefixes().get(0);
+
 
         StringBuilder basicCommands = new StringBuilder();
         basicCommands.append("__**Commands**__").append("\n");
         for (BasicCommand registeredCommand : Bot.getRegisteredCommands().stream().filter(command -> !(command instanceof AdvancedCommand)).collect(Collectors.toList())) {
-            basicCommands.append("`").append(GeneralConfig.get().getPrefixes().get(0)).append(registeredCommand.getAliases().get(0)).append("`").append(", ");
+            basicCommands.append("`").append(prefix).append(registeredCommand.getAliases().get(0)).append("`").append(", ");
         }
         basicCommands.delete(basicCommands.length() - 2, basicCommands.length() - 1);
 
@@ -32,8 +35,14 @@ public class CmdHelp extends DiscordCommand {
             StringBuilder internal = new StringBuilder();
             AdvancedDiscordCommand.getAdvancedCommand(registeredCommand.getClazz()).getSuperCommands().forEach(superCommand -> internal.append(superCommand.getAliases().get(0)).append(" | "));
             internal.delete(internal.length() - 3, internal.length());
-            basicCommands.append("`").append(GeneralConfig.get().getPrefixes().get(0)).append(registeredCommand.getAliases().get(0)).append(" (").append(internal).append(")`").append("\n");
+            basicCommands.append("`").append(prefix).append(registeredCommand.getAliases().get(0)).append(" (").append(internal).append(")`").append("\n");
         }
+
+        basicCommands.append("\n__**Custom Commands**__").append("\n");
+        for (CustomCommandsConfig.MessageSetting cmd : CustomCommandsConfig.get().getCustomCommands()) {
+            basicCommands.append("`" + prefix + cmd.getCallers().get(0) + "`, ");
+        }
+        basicCommands.delete(basicCommands.length() - 2, basicCommands.length() - 1);
 
 
         e.reply(EmbedMaker.builder().author(e.getAuthor()).authorName("DiscordBot Help Page").authorImg(Bot.getJda().getSelfUser().getEffectiveAvatarUrl()).content(basicCommands.toString()).build());
